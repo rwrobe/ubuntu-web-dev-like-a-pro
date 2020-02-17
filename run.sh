bc9e01ee00f751ead58d6f2944acbaf00c10934a
Rob Ward <rwrobe@gmail.com>
2020-02-17 16:33:43 -0500

    Moved user positional

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 FOLDER="$HOME/zorin-webdev-starter-kit"
-ANSIBLE_COMMAND="ansible-playbook $FOLDER/main.yml --ask-sudo-pass -u $2"
+ANSIBLE_COMMAND="ansible-playbook $FOLDER/main.yml --ask-sudo-pass"
 
 provistext="
 ======================================================================
 Update the playbooks as you see fit, and run the following commands:
 $> $FOLDER/run.sh prepare $USER # Install ansible and git; Setup the ansible hosts file for local connections using your shell user.
-$> $FOLDER/run.sh install $USER # Where the magic happens.
+$> $FOLDER/run.sh install # Where the magic happens.
 $> $ANSIBLE_COMMAND # If you prefer to run this using Ansible.
 Warning! If you run 'ansible-playbook' manually. Run it from the user you want to provision. NOT as sudo, script will ask sudo pass when needed!
 ======================================================================"
@@ -31,7 +31,7 @@ case "$1" in
             echo "Clone ubuntu-web-dev-like-a-pro"
             git clone https://github.com/rwrobe/zorin-webdev-starter-kit ~/zorin-webdev-starter-kit
             sudo echo "[local]" >> /etc/ansible/hosts
-            sudo echo "127.0.0.1 ansible_user=$USER" >> /etc/ansible/hosts
+            sudo echo "127.0.0.1 ansible_user=$2" >> /etc/ansible/hosts
             echo "DONE! All required soft is preinstalled"
             echo "$provistext"
             ;;
